120ebfa848736c51b49437cb480651908d4dbb50
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-05 20:52:51 +0300

    refactor: bankAccount to ping in controller

--- src/main/java/com/java/graphql/controller/Ping.java
+++ src/main/java/com/java/graphql/controller/Ping.java
@@ -1,21 +1,13 @@
 package com.java.graphql.controller;
 
-import com.java.graphql.domain.bank.BankAccount;
-import com.java.graphql.domain.bank.Status;
-import com.java.graphql.domain.bank.ping.Ping;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
-import java.util.UUID;
-
-@Slf4j
 @Controller
-public class BankAccountController{
+public class Ping {
     @QueryMapping
-    public Ping ping(){
-        return Ping.pong;
+    public com.java.graphql.domain.bank.ping.Ping ping(){
+        return com.java.graphql.domain.bank.ping.Ping.pong;
     }
 }
