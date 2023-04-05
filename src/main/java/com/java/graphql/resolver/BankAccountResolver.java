9ae09cebb4df8a4b28d5054439ab2b2825c900e5
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-05 15:20:06 +0300

    chore: resolver

---
+++ src/main/java/com/java/graphql/resolver/BankAccountResolver.java
@@ -0,0 +1,9 @@
+package com.java.graphql.resolver;
+
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BankAccountResolver implements GraphQLQueryResolver {
+
+}
