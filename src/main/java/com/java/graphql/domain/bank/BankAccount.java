c37017e82d9d09c29fa677a22f534cb2049f178c
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-08 17:58:13 +0300

    refactor: attribute

--- src/main/java/com/java/graphql/domain/bank/BankAccount.java
+++ src/main/java/com/java/graphql/domain/bank/BankAccount.java
@@ -1,15 +1,30 @@
 package com.java.graphql.domain.bank;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
 @Builder
-public record BankAccount (
-        UUID id,
-        String firstName,
-        String lastName,
-        String phoneNumber,
-        Status status
-){
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankAccount{
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private UUID id;
+    private String ref;
+    private Client client;
+    private Status status;
 }
