5989cd7128b92576a06bcab39b15d53f141f085a
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-10 16:17:42 +0300

    feat: bankAccount service

---
+++ src/main/java/com/java/graphql/service/BankAccountService.java
@@ -0,0 +1,18 @@
+package com.java.graphql.service;
+
+import com.java.graphql.domain.model.BankAccount;
+import com.java.graphql.repository.BankAccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class BankAccountService {
+    private BankAccountRepository repository;
+
+    public List<BankAccount> getBankAccounts(){
+
+    }
+}
