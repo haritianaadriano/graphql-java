adb52a2f4b86c471acb301c9929ada0c35793cc9
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-10 16:21:17 +0300

    feat: query mapping for bankAccount

---
+++ src/main/java/com/java/graphql/controller/BankAccountController.java
@@ -0,0 +1,20 @@
+package com.java.graphql.controller;
+
+import com.java.graphql.domain.model.BankAccount;
+import com.java.graphql.service.BankAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class BankAccountController {
+    private BankAccountService service;
+
+    @QueryMapping
+    public List<BankAccount> getBankAccounts(){
+        return service.getBankAccounts();
+    }
+}
