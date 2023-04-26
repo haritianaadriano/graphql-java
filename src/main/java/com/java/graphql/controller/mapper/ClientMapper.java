b609bacb9442576bd6f00a08d44a1e0e83e56172
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-26 19:50:56 +0300

    chore: initiate the client mapping

---
+++ src/main/java/com/java/graphql/controller/mapper/ClientMapper.java
@@ -0,0 +1,4 @@
+package com.java.graphql.controller.mapper;
+
+public class ClientMapper {
+}
---
+++ src/main/java/com/java/graphql/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.java.graphql.repository;
+
+import com.java.graphql.domain.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+}
