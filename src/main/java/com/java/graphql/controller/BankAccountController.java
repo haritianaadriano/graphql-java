918f6990c8ea37b35116bbd4ef0f08ca271153c7
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-26 20:27:05 +0300

    feat: mutation in bankaccount controller

--- src/main/java/com/java/graphql/controller/BankAccountController.java
+++ src/main/java/com/java/graphql/controller/BankAccountController.java
@@ -1,8 +1,11 @@
 package com.java.graphql.controller;
 
 import com.java.graphql.domain.model.BankAccount;
+import com.java.graphql.domain.model.Status;
 import com.java.graphql.service.BankAccountService;
 import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -16,5 +19,14 @@ public class BankAccountController {
     @QueryMapping
     public List<BankAccount> bankAccount(){
         return service.getBankAccounts();
+    }
+
+    @MutationMapping
+    public BankAccount createBankAccount(
+            @Argument String ref,
+            @Argument Status status,
+            @Argument int client
+    ){
+        return service.createBankAccounts(ref, status, client);
     }
 }
---
+++ src/main/java/com/java/graphql/controller/ClientController.java
@@ -0,0 +1,10 @@
+package com.java.graphql.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class ClientController {
+
+}
