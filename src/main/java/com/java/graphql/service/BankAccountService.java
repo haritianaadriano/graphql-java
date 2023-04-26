623045a946c144465ee1d103ea2f308f84f60050
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-26 20:00:27 +0300

    feat: a service to save a bankaccount

--- src/main/java/com/java/graphql/service/BankAccountService.java
+++ src/main/java/com/java/graphql/service/BankAccountService.java
@@ -1,6 +1,8 @@
 package com.java.graphql.service;
 
+import com.java.graphql.controller.mapper.BankAccountMapper;
 import com.java.graphql.domain.model.BankAccount;
+import com.java.graphql.domain.model.Status;
 import com.java.graphql.repository.BankAccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +13,13 @@ import java.util.List;
 @AllArgsConstructor
 public class BankAccountService {
     private BankAccountRepository repository;
+    private BankAccountMapper mapper;
 
     public List<BankAccount> getBankAccounts(){
         return repository.findAll();
     }
-    public List<BankAccount> createBankAccounts(){
-
+    public BankAccount createBankAccounts(String ref, Status status, int client){
+        com.java.graphql.controller.model.BankAccount parameter = new com.java.graphql.controller.model.BankAccount(ref, status, client);
+        return repository.save(mapper.toData(parameter));
     }
 }
