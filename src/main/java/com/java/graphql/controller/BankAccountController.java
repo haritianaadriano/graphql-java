4bf6ada71ca194b0214d72eea333a815f1aa094c
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-27 09:09:08 +0300

    fix: argument to arguments

--- src/main/java/com/java/graphql/controller/BankAccountController.java
+++ src/main/java/com/java/graphql/controller/BankAccountController.java
@@ -2,10 +2,9 @@ package com.java.graphql.controller;
 
 import com.java.graphql.controller.mapper.BankAccountMapper;
 import com.java.graphql.domain.model.BankAccount;
-import com.java.graphql.domain.model.Status;
 import com.java.graphql.service.BankAccountService;
 import lombok.AllArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -25,7 +24,7 @@ public class BankAccountController {
     }
 
     @MutationMapping
-    public List<BankAccount> createBankAccount(@Argument List<com.java.graphql.controller.model.BankAccount> data){
+    public List<BankAccount> createBankAccount(@Arguments List<com.java.graphql.controller.model.BankAccount> data){
         List<BankAccount> toCreate = data.stream()
                 .map(mapper::toData)
                 .collect(Collectors.toUnmodifiableList());
--- src/main/java/com/java/graphql/controller/ClientController.java
+++ src/main/java/com/java/graphql/controller/ClientController.java
@@ -4,7 +4,7 @@ import com.java.graphql.controller.mapper.ClientMapper;
 import com.java.graphql.domain.model.Client;
 import com.java.graphql.service.ClientService;
 import lombok.AllArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,7 +18,7 @@ public class ClientController {
     private ClientMapper mapper;
 
     @MutationMapping
-    public List<Client> createClient(@Argument List<com.java.graphql.controller.model.Client> data){
+    public List<Client> createClient(@Arguments List<com.java.graphql.controller.model.Client> data){
         List<Client> toCreate = data.stream()
                 .map(mapper::toData)
                 .collect(Collectors.toUnmodifiableList());
