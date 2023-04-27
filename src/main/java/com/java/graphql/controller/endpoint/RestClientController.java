d2060b847276ebc399d9615d83223e5bd878cc92
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-27 08:45:16 +0300

    feat: post mapping for a client

--- src/main/java/com/java/graphql/controller/endpoint/RestClientController.java
+++ src/main/java/com/java/graphql/controller/endpoint/RestClientController.java
@@ -1,20 +1,35 @@
 package com.java.graphql.controller.endpoint;
 
+import com.java.graphql.controller.mapper.ClientMapper;
 import com.java.graphql.domain.model.Client;
 import com.java.graphql.service.ClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 public class RestClientController {
     private ClientService service;
+    private ClientMapper mapper;
 
     @GetMapping("/clients")
     public List<Client> getClients(){
-        return service
+        return service.getClients().stream()
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @PostMapping("/clients")
+    public List<Client> createClients(@RequestBody List<com.java.graphql.controller.model.Client> data){
+        List<Client> toCreate = data.stream().map(mapper::toData)
+                .collect(Collectors.toUnmodifiableList());
+        return service.createClients(toCreate)
+                .stream()
+                .collect(Collectors.toUnmodifiableList());
     }
 }
