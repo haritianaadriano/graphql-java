d580ecc70e7bf5f77410653dc451169059c75c54
haritiana adriano <hei.adriano.4@gmail.com>
2023-04-27 08:55:41 +0300

    feat: some data to test the endpoint

--- src/main/resources/db/migration/V0_3__Data_client_table.sql
+++ src/main/resources/db/migration/V0_3__Data_client_table.sql
@@ -1,2 +1,5 @@
 insert into "client" ( first_name, last_name, phone_number, email ) values
-('first-name-client1', 'last-name-client1', 'phone1', 'email1');
+('first-name-client1', 'last-name-client1', 'phone1', 'email1'),
+('first-name-client2', 'last-name-client2', 'phone2', 'email2'),
+('first-name-client3', 'last-name-client3', 'phone3', 'email3'),
+('first-name-client4', 'last-name-client4', 'phone4', 'email4');
--- src/main/resources/db/migration/V0_4__Data_bankaccount_table.sql
+++ src/main/resources/db/migration/V0_4__Data_bankaccount_table.sql
@@ -1,2 +1,4 @@
 insert into "bank_account" ( ref, client_id, status ) values
-('ref1', 1, 'ENABLE');
+('ref1', 1, 'ENABLE'),
+('ref2', 2, 'ENABLE'),
+('ref3', 3, 'DISABLE');
