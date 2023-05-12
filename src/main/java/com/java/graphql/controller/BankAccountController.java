4b83b9b8f0873b7fc315dc393be222528d35929d
adriano <hei.adriano.4@gmail.com>
2023-05-12 13:29:11 +0300

    refactor: over import in bankaccountcontroller

--- src/main/java/com/java/graphql/controller/BankAccountController.java
+++ src/main/java/com/java/graphql/controller/BankAccountController.java
@@ -5,7 +5,6 @@ import com.java.graphql.domain.model.BankAccount;
 import com.java.graphql.service.BankAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
