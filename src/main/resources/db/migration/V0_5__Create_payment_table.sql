7a3d13769a22117583a858b0e5d7d8c17b03731e
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-09-16 18:07:14 +0300

    fix: table attribute

--- src/main/resources/db/migration/V0_5__Create_payment_table.sql
+++ src/main/resources/db/migration/V0_5__Create_payment_table.sql
@@ -2,6 +2,6 @@ create table if not exists "payment" (
   id serial primary key,
   created_at timestamp,
   is_late boolean,
-  clien_id int references "client"(id),
+  client_id int references "client"(id),
   bank_account_id int references "bank_account"(id)
 );
